483ff71f496e8efd0845d12d962df9db54b5665c
LorenzoDeBiasi <146328126+LorenzoDeBiasi@users.noreply.github.com>
2023-10-27 09:34:58 +0000

    modifiche jack

--- JackSimulationTest/Test.cpp
+++ JackSimulationTest/Test.cpp
@@ -13,20 +13,39 @@ int main(int argc, char * argv[]){
     char line[256];
     char rcorretta[256];
         
-    Questions.open(argv[1], ios::in);
+    
     Answers.open(argv[2], ios::in);
 
-    
-    while(Questions.getline(line, 256)){
-        cout << line << endl;
-        cin >> risposta;
-        Answers.getline(rcorretta, 256);
+    //pescare una riga a caso
+    //idea: aprire e chiudere lo stream ad ogni ciclo, ogni volta si va a guardare la riga
+    //pescata dopo aver incontrato n caratteri di endline, dove n è pescato randomicamente ogni volta
+    for(int i = 0; i < 5; i++){
+        srand(time(0));
+        int n = rand()%10;
+        int counter = 0;
+        
+        Questions.open(argv[1], ios::in);
+        Answers.open(argv[2], ios::in);
 
+        do{
+            Questions.getline(line, 256);
+            Answers.getline(rcorretta, 256);
+            counter ++;
+        }while(counter != n);
+        if(counter == n)
+            counter = 0;
+
+        cout << "\n" << line << " " << rcorretta << "\n";
+
+        cin >> risposta;
         if(rcorretta[0] == risposta[0])
-            cout << "\ngiusto\n";
+            cout << "GIUSTO\n";
         else
-            cout << "\nsbagliato\n";
+            cout << "SBAGLIATO\n";
+
+
+        Questions.close();
+        Answers.close();
     }
-    Questions.close();
-    Answers.close();    
+
 }
